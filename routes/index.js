ff8f9e94e5c538f829096c433182e7dac9985b68
玥唐 <oppaikyouma@163.com>
2016-08-07 22:30:46 +0800

    提交关于ckeditor的demo

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/ckeditorDemo', function(req, res, next) {
+	res.render('ckeditorDemo');
+});
+
 module.exports = router;
